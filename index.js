aa100a5eccff334f29ff5ccdb1f47faa6bc9204a
Juan Formoso <trajano.juan@gmail.com>
2022-01-16 02:28:08 -0300

    Refatorando index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routerTalker = require('./routerTalker');
-const { checkEmail, checkPassword } = require('./authentication');
-const createToken = require('./token');
+const { verifyEmail, verifyPassword } = require('./authentication');
+const generateToken = require('./token');
 
 const app = express();
 app.use(bodyParser.json());
-
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
@@ -17,9 +16,10 @@ app.get('/', (_request, response) => {
 
 app.use('/talker', routerTalker);
 
-app.post('/login', checkEmail, checkPassword, (_request, response) => {
-  response.status(HTTP_OK_STATUS).json({ token: createToken() });
-});
+app.post('/login',
+verifyEmail,
+verifyPassword,
+(_request, response) => response.status(HTTP_OK_STATUS).json({ token: generateToken() }));
 
 app.listen(PORT, () => {
   console.log('Online');
