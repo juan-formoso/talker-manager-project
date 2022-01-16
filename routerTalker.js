1117d31822ab0fa74d82841c9ee92b24f59eb6e8
Juan Formoso <trajano.juan@gmail.com>
2022-01-16 01:34:12 -0300

    Add error route

--- routerTalker.js
+++ routerTalker.js
@@ -4,6 +4,7 @@ const express = require('express');
 const talker = './talker.json';
 const router = express.Router();
 const HTTP_OK_STATUS = 200;
+const HTTP_ERROR_STATUS = 404;
 
 router.get('/', (_request, response) => {
   const data = JSON.parse(fs.readFileSync(talker));
@@ -15,6 +16,10 @@ router.get('/:id', (request, response) => {
   const data = JSON.parse(fs.readFileSync(talker));
   const { id } = request.params;
   const findTalk = data.find((talk) => talk.id === parseInt(id, 10));
+  if (!findTalk) { 
+    return response.status(HTTP_ERROR_STATUS)
+      .json({ message: 'Pessoa palestrante não encontrada' });
+  }
   return response.status(HTTP_OK_STATUS).json(findTalk);
 });
 
