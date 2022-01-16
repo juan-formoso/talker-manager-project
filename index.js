4e3b091a974566638e97fca5e8d017094d51f78c
Juan Formoso <trajano.juan@gmail.com>
2022-01-16 00:52:50 -0300

    Finalizando requisito 1

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+const getTalkers = async () => {
+  const talkers = await fs.readFile('./talkers.json', 'utf8', (err, data) => {
+    if (err) return [];
+    return data;
+  });
+  return JSON.parse(talkers);
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -14,4 +23,9 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (request, response) => {
+  const talkers = await getTalkers();
+  response.status(HTTP_OK_STATUS).send(talkers);
 });
