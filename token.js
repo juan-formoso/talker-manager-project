ae165c75ea73953d9f6cf00f98342ff9f748c5c8
Juan Formoso <trajano.juan@gmail.com>
2022-01-16 02:34:45 -0300

    Refatorando token - 2

--- token.js
+++ token.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 
-function generateToken() {
+function createToken() {
   return crypto.randomBytes(8).toString('hex');
 }
 
-module.exports = generateToken;
+module.exports = createToken;
